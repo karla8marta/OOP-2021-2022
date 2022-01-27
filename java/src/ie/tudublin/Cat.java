ad45ef7984692bc2f76f4011f09ab7f48ac84be0
karla8marta <95685389+karla8marta@users.noreply.github.com>
2022-01-27 16:11:09 +0000

    killing the cat

--- java/src/ie/tudublin/Cat.java
+++ java/src/ie/tudublin/Cat.java
@@ -1,9 +1,27 @@
 package ie.tudublin;
 
-public class Cat extends Animal
-{
-    public Cat(String name)
-    {
+public class Cat extends Animal {
+    int numLives;
+
+    public int getNumLives() {
+        return this.numLives;
+    }
+
+    public void setNumlives(int numLives) {
+        this.numLives = numLives;
+    }
+
+    public Cat(String name) {
         super(name);
+        numLives = 9;
+    }
+
+    public void killMe() {
+        if (this.numLives <= 0) {
+            System.out.println("Dead.");
+        } else {
+            System.out.println("Ouch.");
+            this.numLives -= 1;
+        }
     }
 }
