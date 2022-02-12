fbb7a3f7d21f4e20ca9fb0a9e43dba1c0823f141
karla8marta <95685389+karla8marta@users.noreply.github.com>
2022-02-12 12:38:09 +0000

    hello

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -2,21 +2,33 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 
-public class Loops extends PApplet
-{
+public class Loops extends PApplet {
 
-	public void settings()
-	{
+	int mode = 0;
+
+	public void settings() {
 		size(500, 500);
 	}
 
 	public void setup() {
 		colorMode(HSB);
-		
+
 	}
 
-	
-	public void draw()
-	{	
+	public void keyPressed() {
+		if (key >= '0' && key <= '9') {
+			mode = key - '0';
+		}
+		println(mode);
+	}
+
+	public void draw() {
+
+		switch (mode) {
+			case 0:
+				break;
+			case 1:
+				break;
+		}
 	}
 }
