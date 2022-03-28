44cdf661dde0675f6a12e5786277b41f2ce31177
karla8marta <95685389+karla8marta@users.noreply.github.com>
2022-03-28 17:23:05 +0100

    h

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -58,19 +58,16 @@ public class Arrays extends PApplet {
 
     }
 
-    public void draw()
-    {
+    public void draw() {
         switch (mode) {
-			case 0:
-            {
+            case 0: {
                 background(0);
                 colorMode(HSB);
-                float w = width / (float)rainfall.length;
+                float w = width / (float) rainfall.length;
                 noStroke();
-                for(int i = 0 ; i < rainfall.length ; i ++)
-                {
+                for (int i = 0; i < rainfall.length; i++) {
                     float x = map(i, 0, rainfall.length, 0, width);
-                    int c = (int)map(i, 0, rainfall.length, 0, 255);
+                    int c = (int) map(i, 0, rainfall.length, 0, 255);
                     fill(c, 255, 255);
                     float h = map(rainfall[i], 0, rainfall[maxIndex], 0, -height);
                     rect(x, height, w, h);
@@ -80,35 +77,33 @@ public class Arrays extends PApplet {
                 }
                 break;
             }
-            case 1:
-            {
+            case 1: {
                 background(0);
                 float border = width * 0.1f;
                 // Draw the axis
                 stroke(255);
                 line(border, border, border, height - border);
                 line(border, height - border, width - border, height - border);
-                for(int i = 0 ; i <= 120; i += 10)
-                {
+
+                for (int i = 0; i <= 120; i += 10) {
                     float y = map(i, 0, 120, height - border, border);
                     line(border - 5, y, border, y);
                     fill(255);
                     textAlign(CENTER, CENTER);
                     text(i, border / 2, y);
                 }
-                float w = (width - (border * 2.0f)) / (float)rainfall.length;
-                
-                for(int i = 0 ; i < rainfall.length; i ++)
-                {
-                    float x = map(i, 0, rainfall.length, border, width-border);
+                float w = (width - (border * 2.0f)) / (float) rainfall.length;
+
+                for (int i = 0; i < rainfall.length; i++) {
+                    float x = map(i, 0, rainfall.length, border, width - border);
                     float c = map(i, 0, rainfall.length, 0, 255);
                     stroke(255);
                     fill(c, 255, 255);
-                    float h = map(rainfall[i], 0, 120, 0, -height + (border * 2.0f)); 
-                    rect(x, height-border, w, h);
+                    float h = map(rainfall[i], 0, 120, 0, -height + (border * 2.0f));
+                    rect(x, height - border, w, h);
                     fill(255);
                     text(months[i], x + (w / 2), height - (border / 2));
-    
+
                 }
                 break;
             }
@@ -119,31 +114,29 @@ public class Arrays extends PApplet {
                 float cy = height / 2;
                 stroke(255);
                 noFill();
-                //circle(cx, cy, r * 2.0f);
+                // circle(cx, cy, r * 2.0f);
                 float tot = 0;
-                for(float f:rainfall)
-                {
+                for (float f : rainfall) {
                     tot += f;
                 }
                 float start = 0;
-                for(int i = 0 ; i < rainfall.length ; i ++)
-                {
+                for (int i = 0; i < rainfall.length; i++) {
                     float val = map(rainfall[i], 0, tot, 0, TWO_PI);
                     float c = map(i, 0, rainfall.length, 0, 255);
                     noStroke();
                     fill(c, 255, 255);
                     arc(cx, cy, r * 2, r * 2, start, start + val, PIE);
-                    
+
                     float theta = start + (val * 0.5f);
                     float x = cx + cos(theta) * (r * 1.2f);
                     float y = cy + sin(theta) * (r * 1.2f);
                     fill(255);
                     text(months[i], x, y);
                     start = start + val;
-                    
+
                 }
 
                 break;
-            }        
+        }
     }
 }
--- java/src/ie/tudublin/Life.java
+++ java/src/ie/tudublin/Life.java
@@ -3,12 +3,11 @@ package ie.tudublin;
 import processing.core.PApplet;
 
 public class Life extends PApplet {
-	
+
 	LifeBoard board;
 
-	public void mouseDragged()
-	{
-		//int col = (int) (mouseX / (board.cellSize));
+	public void mouseDragged() {
+		// int col = (int) (mouseX / (board.cellSize));
 
 		int col = (int) map(mouseX, 0, width, 0, board.size);
 		int row = (int) map(mouseY, 0, width, 0, board.size);
@@ -16,11 +15,10 @@ public class Life extends PApplet {
 		board.setAlive(row, col, true);
 	}
 
-
 	public void settings() {
 		size(1000, 1000);
 		board = new LifeBoard(200, this);
-		//board.randomise();
+		// board.randomise();
 		println(board.countCellsAround(1, 1));
 	}
 
@@ -28,7 +26,7 @@ public class Life extends PApplet {
 		colorMode(RGB);
 
 	}
-	
+
 	public void draw() {
 		board.render();
 		board.update();
